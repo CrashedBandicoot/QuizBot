b2f1983efbc235d7346657b2bd6d8954769807a7
Thuyet Ta <thuyetta.tom@gmail.com>
2021-04-28 19:56:07 -0700

    merge

--- sumy_example.py
+++ sumy_example.py
@@ -1,10 +1,6 @@
 from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
-
-
-
-
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
@@ -28,5 +24,4 @@ if __name__ == "__main__":
     summarizer.stop_words = get_stop_words(LANGUAGE)
 
     for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        print("=============")
         print(sentence)
